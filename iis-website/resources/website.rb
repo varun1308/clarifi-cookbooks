3d6d79aded8a62281821cdcd1347a8de801a1e75
VARUN JAIN <varun13@gmail.com>
2015-10-21 09:20:48 +0530

    website resource to iis-website cookbook

---
+++ iis-website/resources/website.rb
@@ -0,0 +1,42 @@
+attribute :website_name, :name_attribute => true, :kind_of => String, :required => true
+attribute :website_source, :kind_of => String
+attribute :host_header, :kind_of => String
+attribute :port, :kind_of => Fixnum, :default => 80
+attribute :protocol, :kind_of => Symbol, :equal_to => [:http, :https],  :default => :http
+attribute :website_base_directory, :kind_of => String, :default => "#{ENV['SYSTEMDRIVE']}\\inetpub\\wwwroot"
+
+action :add do
+	
+	site_directory = "#{website_base_directory}\\#{website_name}"
+	
+	app_pool_name = website_name
+	# Download the built application and unzip it to the app directory.
+	windows_zipfile site_directory do
+	  source website_source
+	  action :unzip
+	  not_if { ::File.exists?(site_directory) }
+	end
+
+	# Create the site app pool.
+	iis_pool  website_name do
+	  runtime_version '4.0'
+	  action :add
+	end
+
+	# Create the site directory and give IIS_IUSRS read rights.
+	directory site_directory do
+	  rights :read, 'IIS_IUSRS'
+	  recursive true
+	  action :create
+	end
+
+	# Create the app site.
+	iis_site website_name do
+	  protocol protocol
+	  port port
+	  path site_directory
+	  application_pool website_name
+	  host_header host_header
+	  action [:add, :start]
+	end	
+end

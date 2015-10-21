73a1d4a45be89a0406ebac0f8638bff60a435a1d
VARUN JAIN <varun13@gmail.com>
2015-10-21 16:54:48 +0530

    fixed kind_of attribute

--- tavisca_apps/resources/winservice.rb
+++ tavisca_apps/resources/winservice.rb
@@ -5,5 +5,5 @@ attribute :service_name, :name_attribute => true, :kind_of => String, :required 
 attribute :service_source, :kind_of => String, :required => true
 attribute :service_executable_with_args, :kind_of => String, :required => true
 attribute :service_install_base_path, :kind_of => String, :default => "#{ENV['SYSTEMDRIVE']}\\winservices"
-attribute :service_start, kind_of => Symbol, :equal_to => [:automatic, :disabled, :manual],  :default => :manual
+attribute :service_start, :kind_of => Symbol, :equal_to => [:automatic, :disabled, :manual],  :default => :manual
 

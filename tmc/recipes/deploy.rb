806cc7fd9c254b499a8444b6ae3df2c7df7b44f8
VARUN JAIN <varun13@gmail.com>
2015-10-21 09:30:59 +0530

    refactored tmc deploy recipe

--- tmc/recipes/deploy.rb
+++ tmc/recipes/deploy.rb
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 # Define the local app and site locations.
+require 'iis_website'
+
 Chef::Log.level = :debug
 site_name = node['tmc']['site_name']
 site_source = node['tmc']['site_source']

5e0c72753140549723d1a97f5ce89438e4359c4b
VARUN JAIN <varun13@gmail.com>
2015-10-22 12:03:03 +0530

    added logging to scm provide

--- opsworks_scm/libraries/opsworks_scm.rb
+++ opsworks_scm/libraries/opsworks_scm.rb
@@ -13,16 +13,9 @@ module OpsWorks
         virtual_host_match = uri.host.match(/\A((.+)\.)?s3(?:-(?:ap|eu|sa|us)-.+-\d)?\.amazonaws\.com/i)
         Chef::Log.info "virtual_host_match: #{virtual_host_match}"
 
-        if virtual_host_match
-          # virtual-hosted-style: http://bucket.s3.amazonaws.com or http://bucket.s3-aws-region.amazonaws.com
-          bucket = virtual_host_match[1]
-          remote_path = uri_path_components.join("/")
-        else
-          # path-style: http://s3.amazonaws.com/bucket or http://s3-aws-region.amazonaws.com/bucket
-          bucket = uri_path_components[0]
-          remote_path = uri_path_components[1..-1].join("/")
-        end
-
+        bucket = uri_path_components[0]
+        remote_path = uri_path_components[1..-1].join("/")
+        
         [bucket, remote_path]
       end
     end

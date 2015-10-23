c765a328bda91ab6818ad2f3096579bedc386fa7
VARUN JAIN <varun13@gmail.com>
2015-10-23 14:57:31 +0530

    added fixes for changes in room_std

--- clarifi/recipes/room_std_website.rb
+++ clarifi/recipes/room_std_website.rb
@@ -4,12 +4,22 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-tavisca_apps_website node['room_std_website']['site_name'] do
-  website_source node['room_std_website']['site_source']
-  host_header node['room_std_website']['host_header']
-  port node['room_std_website']['port']
-  protocol node['room_std_website']['protocol']
-  website_base_directory node['room_std_website']['website_base_directory']
-  runtime_version node['room_std_website']['runtime_version']
-  action :add
+Chef::Log.level = :debug
+
+apps = search(:aws_opsworks_app, "deploy:true") rescue []
+app = apps.find {|x| x[:shortname] == "room_std"}
+if app
+	Chef::Log.debug "Found #{app[:shortname]} to deploy on the stack. Assuming room_std app is same."
+
+	tavisca_apps_website node['room_std']['site_name'] do
+	  host_header node['room_std']['host_header']
+	  port node['room_std']['port']
+	  protocol node['room_std']['protocol']
+	  website_base_directory node['room_std']['site_base_directory']
+	  runtime_version node['room_std']['runtime_version']
+	  scm app["app_source"]
+	  action :add
+	end
+else
+	Chef::Log.debug "room_std app not found in apps to deploy."
 end

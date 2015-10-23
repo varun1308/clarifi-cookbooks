9982b86f88a0e7c9e787e156fb971d1cec70b0ca
VARUN JAIN <varun13@gmail.com>
2015-10-23 21:17:19 +0530

    removed back to rubycopy

--- tavisca_apps/providers/website.rb
+++ tavisca_apps/providers/website.rb
@@ -1,7 +1,5 @@
 use_inline_resources
 
-require 'fileutils'
-
 action :add do
 	
 	website_directory = "#{new_resource.website_base_directory}\\#{new_resource.website_name}"
@@ -23,43 +21,10 @@ action :add do
 	    type             new_resource.scm[:type]
   	end
 
-  	#apply web.configchanges
-  	#check if file needs to be replaced
-  	if new_resource.should_replace_web_config && new_resource.new_web_config.empty? == false
-  		
-  		if ::File.exist?("#{app_checkout}\\web.config")
-  			Chef::Log.debug "old web.config filepath: #{app_checkout}\\web.config. exists"
-  		else
-  			Chef::Log.debug "old web.config filepath: #{app_checkout}\\web.config does not exist "
-		end
-  		#remove old web.config
-  		# ::FileUtils.rm "#{app_checkout}\\web.config", :force => true
-
-  		#move the new.web.config file to web.config
-  		#::FileUtils.mv "#{app_checkout}\\#{new_resource.new_web_config}", "#{app_checkout}\\web.config", { :force => true, :verbose => true }
-
-  	elsif new_resource.web_erb_config.empty? == false #if erb file is defined
-  		#apply template to create web.config
-  		template "#{app_checkout}\\web.config" do
-  		  local true
-		  source "#{new_resource.web_erb_config}"
-		  mode '0755'
-		end
-  	end
-
-  	# Copy app to deployment directory
-
-  	#delete destination dir
-  	::FileUtils.rm_r "#{website_directory}", :force => true
-  	#copy source dir to base dir
-	::FileUtils.cp_r "#{app_checkout}", "#{new_resource.website_base_directory}"
-	#delete source dir
-  	::FileUtils.rm_r "#{app_checkout}", :force => true
-
-
-	# execute "copy #{new_resource.website_name}" do
-	# 	command "Robocopy.exe #{app_checkout} #{website_directory} /MIR /XF .gitignore /XF web.config.erb /XD .git"
-	# end
+  		# Copy app to deployment directory
+	execute "copy #{new_resource.website_name}" do
+		command "Robocopy.exe #{app_checkout} #{website_directory} /MIR /XF .gitignore /XF web.config.erb /XD .git"
+	end
 	
 	# Create the site app pool.
 	iis_pool  new_resource.website_name do
@@ -72,8 +37,6 @@ action :add do
 	  recursive true
 	  action :create
 	end
-
-
 
 	# Create the app site.
 	iis_site new_resource.website_name do

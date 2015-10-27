4847dc9d99c77ba323f15e6e6d5a9462f2189f21
VARUN JAIN <varun13@gmail.com>
2015-10-27 17:02:27 +0530

    removed file rights from s3_file

--- clarifi/recipes/cert_install.rb
+++ clarifi/recipes/cert_install.rb
@@ -3,6 +3,8 @@
 # Recipe:: cert_install
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+Chef::Log.level = :debug
+
 s3_file File.join(node["cert_install"]['local_dir'], node["cert_install"]['remote_path']) do
 	bucket node["cert_install"]['bucket']
 	remote_path node["cert_install"]['remote_path']

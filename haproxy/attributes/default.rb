f5f882fdc7c8988fdd69d23144aefcd76dad6abe
VARUN JAIN <varun13@gmail.com>
2015-10-27 15:12:05 +0530

    added ssl cert download

---
+++ haproxy/attributes/default.rb
@@ -0,0 +1,7 @@
+default['cert_install']['local_dir'] = '/home/crt_dir/'
+default['cert_install']['remote_path'] = 'certs.zip'
+default['cert_install']['bucket'] = 'varun-iis-cookbook'
+default['cert_install']['s3_url'] = 'https://s3-us-west-2.amazonaws.com/varun-iis-cookbook/'
+default['cert_install']['aws_access_key_id'] = ''
+default['cert_install']['aws_secret_access_key'] = ''
+
---
+++ haproxy/recipes/cert_install.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: haproxy
+# Recipe:: cert_install
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+s3_file File.join(node["cert_install"]['local_dir'], node["cert_install"]['remote_path']) do
+	bucket node["cert_install"]['bucket']
+	remote_path node["cert_install"]['remote_path']
+	s3_url node["cert_install"]['s3_url']
+	aws_access_key_id node["cert_install"]['aws_access_key_id']
+	aws_secret_access_key node["cert_install"]['aws_secret_access_key']
+	retries 2
+end
+
+execute "unzip #{node["cert_install"]['local_file']}" do
+    command "unzip #{node["cert_install"]['local_file']}"
+    cwd node["cert_install"]['local_dir']
+end
